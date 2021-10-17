7bdf1b319deb26f81ce0115dc06fd8dd6606bd5c
wooyong2 <woo8355@gmail.com>
2021-10-17 17:23:23 +0900

    add Heap Sort

--- Sort.py
+++ Sort.py
@@ -37,7 +37,7 @@ print(a)
 '''
 
 '''
-# 퀵 정렬 (Quick Sort): O(NlogN)     파이썬의 list.sort()함수나 자바의 Arrays.sort()에 쓰일 정도로 효율적인 알고리즘.
+# 퀵 정렬 (Quick Sort): O(NlogN)    파이썬의 list.sort()함수나 자바의 Arrays.sort()에 쓰일 정도로 효율적인 알고리즘.
 #                                   하지만 pivot 값을 기준으로 분할했을 때 값들이 한 편으로 치우치게되면 성능이 저하되며
 #                                   최악의 경우 한 편으로만 모든 값들이 몰리게 되어 O(N^2)까지도 가능.
 arr = [1, 10, 5, 8, 7, 6, 4, 3, 2, 9]
@@ -71,6 +71,7 @@ print(arr)
 '''
 
 '''
+# 병합 정렬 (Merge Sort): O(NlogN)      Quick Sort와 다르게 어떤 경우에도 NlogN의 시간 복잡도를 가짐.
 def merge_sort(arr):
     def sort(low, high):
         if high - low < 2:
@@ -106,4 +107,49 @@ def merge_sort(arr):
 a = [2, 4, 5, 1, 9, 8, 7, 3]
 merge_sort(a)
 print(a)
+'''
+
+'''
+# 힙 정렬 (Heap Sort): O(NlogN)
+
+# 힙 정렬 코드 출처: 이수진님 블로그 (https://lsjsj92.tistory.com/472)
+import random
+
+# 최대 힙 성질을 만족하는지 확인하고 최대 힙 성질에 맞게 재정렬 해주는 함수
+def heapify(unsorted, index, heap_size):
+    # 정렬 과정 보고 싶다면 주석 처리 해제
+    # print("unsorted:", unsorted)
+    largest = index
+    left_index = 2 * index + 1
+    right_index = 2 * index + 2
+    if left_index < heap_size and unsorted[left_index] > unsorted[largest]:
+        largest = left_index
+    if right_index < heap_size and unsorted[right_index] > unsorted[largest]:
+        largest = right_index
+    # largest의 index가 바뀌었다는 것은 자식 노드의 값이 더 크다는 뜻이므로 부모 노드와 자식 노드의 값 변경하고
+    # 변경되었으므로 부모 노드가 옮겨간 위치(원래 자식 노드의 위치)를 기준으로 다시 heapify 재귀 호출
+    if largest != index:
+        unsorted[largest], unsorted[index] = unsorted[index], unsorted[largest]
+        heapify(unsorted, largest, heap_size)
+
+def heap_sort(unsorted):
+    n = len(unsorted)
+    # heapify의 경우 n개 노드 전체 다 할 필요 없이 n // 2 번째부터만 실행해도 heap의 구조를 갖춤.
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(unsorted, i, n)
+    # 위의 반복문을 거치고 나면 입력 받은 리스트는 최대 힙 구조로 되어 있음.
+
+    # 최대 힙의 성질을 이용해서 루트 노드와 말단 노드의 값을 변경하는 방법으로 큰 순서대로 위치를 고정하며 오름차순 정렬을 하는 반복문
+    for i in range(n - 1, 0, -1):
+        # 가장 큰 값이 말단 노드로 가게 root 값과 말단 노드의 값을 변경 (최대 힙 구조이므로 원래 root에 가장 큰 값이 들어있었음)
+        unsorted[0], unsorted[i] = unsorted[i], unsorted[0]
+        # 가장 큰 값이 들어간 말단 노드를 제외하고 다시 최대 힙 만들어주기위해 heapify 함수 호출
+        heapify(unsorted, 0, i)
+
+    # 정렬이 완료된 리스트 반환 (이름은 unsorted지만 sorted...)
+    return unsorted
+
+test_list = [random.randint(1, 20) for i in range(20)]
+print("original list:", test_list)
+print("sorted list", heap_sort(test_list))
 '''
